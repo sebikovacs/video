af0ea36a62b88f302a60db40f77fcf5eade94d2b
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-01-31 15:10:46 +0200

    Modularizing

--- js/range.js
+++ js/range.js
@@ -1,90 +1,104 @@
-$(document).ready(function () {
-	var range = $('.range-holder'),
-		rangeTrack = $('.range-track'),
-		rangeSelection = $('.range-selection'),
-		rangeSelectionWidth = rangeSelection.width(),
-		time1 = range.find('.time1'),
-		time2 = range.find('.time2'),
-		handle1 = range.find('.range-handle:first'),
-		handle2 = range.find('.range-handle:last'),
-		isDragged = false,
-		draggedRange = 0, 
-		values = [0,0], 
-		whichHandle;
+(function function_name ($, window) {
+	'use strict';
 
-	
-	time1.html(values[0]);
-	time2.html(values[1]);
+	$.fn.rangeSelector = function (options) {
+		var range = $(this),
+			rangeTrack = $('.range-track'),
+			rangeSelection = $('.range-selection'),
+			rangeSelectionWidth = rangeSelection.width(),
+			time1 = range.find('.time1'),
+			time2 = range.find('.time2'),
+			handle1 = range.find('.range-handle:first'),
+			handle2 = range.find('.range-handle:last'),
+			isDragged = false,
+			draggedRange = 0, 
+			values = [0,0], 
+			whichHandle;
 
-	var mousemove = function (e) {
-			
-		if (isDragged) {
-			getRange(e);
-			if (whichHandle == 0 && values[1] < draggedRange) {
-				values[0] = values[1];
-				whichHandle = 1;
-			} else if (whichHandle == 1 && values[0] > draggedRange){
-				values[1] = values[0];
-				whichHandle = 0;
+		
+		time1.html(values[0]);
+		time2.html(values[1]);
+
+		var mousemove = function (e) {
+				
+			if (isDragged) {
+				getRange(e);
+				if (whichHandle == 0 && values[1] < draggedRange) {
+					values[0] = values[1];
+					whichHandle = 1;
+				} else if (whichHandle == 1 && values[0] > draggedRange){
+					values[1] = values[0];
+					whichHandle = 0;
+				}
+				values[whichHandle] = draggedRange;
+				
+				buildLayout();
 			}
-			values[whichHandle] = draggedRange;
-			console.log(values)
-			buildLayout();
+			
+		};
+		
+		var mouseup = function () {
+			isDragged = false;
 		}
 		
-	};
-	
-	var mouseup = function () {
-		isDragged = false;
-	}
-	
-	var mousedown = function (e) {
+		var mousedown = function (e) {
 
-		isDragged = true;
-		getRange(e);
-		
-		var diff1 = Math.abs(draggedRange - values[0]);
-		var diff2 = Math.abs(draggedRange - values[1]);
-		whichHandle = diff1 < diff2 ? 0 : 1;
-		
-		
-		values[whichHandle] = draggedRange;
-		
-		buildLayout();
+			isDragged = true;
+			getRange(e);
+			
+			var diff1 = Math.abs(draggedRange - values[0]);
+			var diff2 = Math.abs(draggedRange - values[1]);
+			whichHandle = diff1 < diff2 ? 0 : 1;
+			
+			
+			values[whichHandle] = draggedRange;
+			
+			buildLayout();
+
+			range.on({
+				mousemove: mousemove
+			})
+		};
+
+		var getRange = function (e) {
+			var pos = e.pageX;
+			var max = rangeTrack.offset().left + rangeTrack[0].offsetWidth;
+			var min = rangeTrack.offset().left;
+			
+			draggedRange = Math.min(Math.max(min, pos), max) - rangeTrack.offset().left;
+		};
+
+		var buildLayout = function () {
+
+			handle1[0].style.left = values[0] + 'px';
+			handle2[0].style.left = values[1] + 'px';
+
+			rangeSelection[0].style.left = values[0] + 'px';
+			rangeSelection.width(values[1] - values[0]);
+
+			//update badge
+			var duration = 3*60 + 11;
+			var inc = rangeTrack.width() / duration;
+
+			time1.html( parseInt(values[0] / inc));
+			time2.html( parseInt(values[1] / inc));
+
+			var rangeValues = {
+				t1: parseInt(values[0] / inc),
+				t2: parseInt(values[1] / inc)
+			}
+
+			window.rangeValues = rangeValues;
+
+		};
 
 		range.on({
-			mousemove: mousemove
-		})
-	};
+			mousedown: mousedown,
+			mouseup: mouseup
+		});
+	}
 
-	var getRange = function (e) {
-		var pos = e.pageX;
-		var max = rangeTrack.offset().left + rangeTrack[0].offsetWidth;
-		var min = rangeTrack.offset().left;
-		
-		draggedRange = Math.min(Math.max(min, pos), max) - rangeTrack.offset().left;
-	};
 
-	var buildLayout = function () {
 
-		handle1[0].style.left = values[0] + 'px';
-		handle2[0].style.left = values[1] + 'px';
-
-		rangeSelection[0].style.left = values[0] + 'px';
-		rangeSelection.width(values[1] - values[0]);
-
-		//update badge
-		var duration = 3*60 + 11;
-		var inc = rangeTrack.width() / duration;
-
-		time1.html( parseInt(values[0] / inc));
-		time2.html( parseInt(values[1] / inc));
-
-	};
-
-	range.on({
-		mousedown: mousedown,
-		mouseup: mouseup
-	});
-
-});
+})(jQuery, window);
+	
--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -176,50 +176,50 @@ $(document).ready(function () {
 		}, 100);
 	})
 	
-	var updateStartPosition = function (e) {
-		var leftCursor = $('.cursor-left');
-		var rightCursor = $('.cursor-right');
-		var range = $('.range');
-		var pixelCount = vid.duration / $('.range').width();
+	// var updateStartPosition = function (e) {
+	// 	var leftCursor = $('.cursor-left');
+	// 	var rightCursor = $('.cursor-right');
+	// 	var range = $('.range');
+	// 	var pixelCount = vid.duration / $('.range').width();
 
-		var left = (e.pageX - range.offset().left) - leftCursor.width();
+	// 	var left = (e.pageX - range.offset().left) - leftCursor.width();
 
-		if ((isDownLeft && left >= 0) && (isDownLeft && left <= 400)) {
-			var time = left * pixelCount;
-			vid.currentTime = time;
+	// 	if ((isDownLeft && left >= 0) && (isDownLeft && left <= 400)) {
+	// 		var time = left * pixelCount;
+	// 		vid.currentTime = time;
 
-			leftCursor.css('left', left + 'px');
-			rightCursor.css('left', (left + 1) + 'px');
-		}
-	};
+	// 		leftCursor.css('left', left + 'px');
+	// 		rightCursor.css('left', (left + 1) + 'px');
+	// 	}
+	// };
 
-	var updateEndPosition = function (e) {
+	// var updateEndPosition = function (e) {
 
-		var leftCursor = $('.cursor-left');
-		var rightCursor = $('.cursor-right');
-		var range = $('.range');
-		var pixelCount = vid.duration / $('.range').width();
+	// 	var leftCursor = $('.cursor-left');
+	// 	var rightCursor = $('.cursor-right');
+	// 	var range = $('.range');
+	// 	var pixelCount = vid.duration / $('.range').width();
 
-		var left = (e.pageX - range.offset().left) - leftCursor.width();
+	// 	var left = (e.pageX - range.offset().left) - leftCursor.width();
 
-		if ((isDownRight && left >= 0) && (isDownRight && left <= 400)) {
+	// 	if ((isDownRight && left >= 0) && (isDownRight && left <= 400)) {
 			
-			var time = left * pixelCount;
-			vid.currentTime = time;
+	// 		var time = left * pixelCount;
+	// 		vid.currentTime = time;
 
-			rightCursor.css('left', (left + 2) + 'px');
-		}
-	}
+	// 		rightCursor.css('left', (left + 2) + 'px');
+	// 	}
+	// }
 
-	$(document).on('mousedown', '.cursor-inner-left', function () { isDownLeft = true;})
-		.on('mouseup', '.cursor-inner-left', function () { isDownLeft = false;})
-		.on('mousemove','.cursor-inner-left', updateStartPosition)
-		.on('mouseup', function () { isDownLeft = false;})
-		.on('mousemove', updateStartPosition);
+	// $(document).on('mousedown', '.cursor-inner-left', function () { isDownLeft = true;})
+	// 	.on('mouseup', '.cursor-inner-left', function () { isDownLeft = false;})
+	// 	.on('mousemove','.cursor-inner-left', updateStartPosition)
+	// 	.on('mouseup', function () { isDownLeft = false;})
+	// 	.on('mousemove', updateStartPosition);
 
-	$(document).on('mousedown', '.cursor-inner-right', function () { isDownRight = true;})
-		.on('mouseup', '.cursor-inner-right', function () { isDownRight = false;})
-		.on('mousemove','.cursor-inner-right', updateEndPosition)
-		.on('mouseup', function () { isDownRight = false;})
-		.on('mousemove', updateEndPosition);
+	// $(document).on('mousedown', '.cursor-inner-right', function () { isDownRight = true;})
+	// 	.on('mouseup', '.cursor-inner-right', function () { isDownRight = false;})
+	// 	.on('mousemove','.cursor-inner-right', updateEndPosition)
+	// 	.on('mouseup', function () { isDownRight = false;})
+	// 	.on('mousemove', updateEndPosition);
 });
